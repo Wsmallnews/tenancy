52bd68f632ca149ab26bc9feccc26cc44e8c3ab4
smallnews@qq.com <1371606921@qq.com>
2025-09-02 18:50:26 +0800

    优化论文 infolist

--- app/Filament/Resources/Theses/Schemas/ThesisInfolist.php
+++ app/Filament/Resources/Theses/Schemas/ThesisInfolist.php
@@ -7,7 +7,10 @@ use Filament\Infolists;
 use Filament\Schemas;
 use Filament\Schemas\Schema;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Str;
 use Filament\Support\Enums\IconPosition;
+use Spatie\MediaLibrary\MediaCollections\Models\Collections\MediaCollection;
+use Spatie\MediaLibrary\MediaCollections\Models\Media;
 
 class ThesisInfolist
 {
@@ -16,37 +19,109 @@ class ThesisInfolist
         return $schema
             ->components([
                 Schemas\Components\Flex::make([
-                    Schemas\Components\Group::make()->schema([
-                        Schemas\Components\Section::make('基础信息')
-                            ->schema([
-                                Infolists\Components\TextEntry::make('thesisType.name')
-                                    ->label('论文类型'),
+                    Schemas\Components\Group::make()
+                        ->schema([
+                            Schemas\Components\Section::make('基础信息')
+                                ->schema([
+                                    Infolists\Components\TextEntry::make('thesisType.name')
+                                        ->label('论文类型'),
+                                    
+                                    Infolists\Components\TextEntry::make('title')
+                                        ->label('标题'),
+                                        
+                                    Infolists\Components\TextEntry::make('author_name')
+                                        ->label('作者'),
+                                        
+                                    Infolists\Components\TextEntry::make('company.name')
+                                        ->label('所属单位')
+                                        ->formatStateUsing(fn (Model $record, $state) => $record->company ? "{$record->company->name} (编号：{$record->company->code})" : '-'),
+                                    Infolists\Components\TextEntry::make('description')
+                                        ->label('摘要')
+                                        ->columnSpanFull(),
+                                        
+                                    Infolists\Components\TextEntry::make('remark')
+                                        ->label('备注')
+                                        ->columnSpanFull(),
+                                ])
+                                ->columns(2),
                                 
-                                Infolists\Components\TextEntry::make('title')
-                                    ->label('标题'),
-                                    
-                                Infolists\Components\TextEntry::make('author_name')
-                                    ->label('作者'),
-                                    
-                                Infolists\Components\TextEntry::make('company.name')
-                                    ->label('所属单位')
-                                    ->formatStateUsing(fn (Model $record, $state) => $record->company ? "{$record->company->name} (编号：{$record->company->code})" : '-'),
-                                Infolists\Components\TextEntry::make('description')
-                                    ->label('摘要')
-                                    ->columnSpanFull(),
-                                    
-                                Infolists\Components\TextEntry::make('remark')
-                                    ->label('备注')
-                                    ->columnSpanFull(),
-                            ])
-                            ->columns(2),
-                            
-                        Schemas\Components\Section::make('附件管理')
-                            ->schema([
-                                Infolists\Components\SpatieMediaLibraryImageEntry::make('theses')
-                                    ->collection('theses')
-                            ]),
-                    ])->columns(1),
+                            Schemas\Components\Section::make('附件管理')
+                                ->schema(function (Model $record) {
+                                    $schemas = [];
+
+                                    $collection = 'theses';
+                                    $medias = $record->getRelationValue('media');
+
+                                    $files = $medias
+                                        ->filter(fn ($media) => 
+                                            $media->getAttributeValue('collection_name') === $collection && Str::doesntStartWith($media->mime_type, 'image/')
+                                        )
+                                        ->sortBy('order_column');
+
+                                    $images = $medias
+                                        ->filter(fn ($media) => 
+                                            $media->getAttributeValue('collection_name') === $collection && Str::startsWith($media->mime_type, 'image/')
+                                        )
+                                        ->sortBy('order_column');
+
+                                    if ($files->isNotEmpty()) {
+                                        // 文件列表
+                                        $schemas[] = Schemas\Components\UnorderedList::make(function () use ($files) {
+                                            return $files->map(function ($media) {
+                                                return Schemas\Components\Text::make($media->name);
+                                            })->all();
+                                        });
+                                    }
+
+                                    if ($images->isNotEmpty()) {
+                                        // 图片
+                                        $schemas[] = Infolists\Components\SpatieMediaLibraryImageEntry::make('theses')
+                                            ->label('图片')
+                                            ->collection('theses')
+                                            ->filterMediaUsing(
+                                                fn ($media): MediaCollection => $media->filter(function ($item) {
+                                                    return Str::startsWith($item->mime_type, 'image/');
+                                                })
+                                            );
+                                    }
+
+                                    return $schemas;
+                                })
+
+
+                                // ->schema([
+                                //     Schemas\Components\UnorderedList::make(function (Model $record) {
+                                //         $collection = 'theses';
+                                //         $medias = $record->getRelationValue('media')
+                                //             ->when(
+                                //                 $collection,
+                                //                 fn (MediaCollection $mediaCollection) => $mediaCollection->filter(fn (Media $media): bool => $media->getAttributeValue('collection_name') === $collection),
+                                //             )
+                                //             ->sortBy('order_column');
+
+                                //         $list = $medias->map(function ($media) {
+                                //             if (strpos($media->mime_type, 'image/') === 0) {
+                                //                 return Schemas\Components\Image::make(
+                                //                     url: $media->getUrl(),
+                                //                     alt: 'QR code to scan with an authenticator app',
+                                //                 );
+                                //             } else {
+                                //                 return Schemas\Components\Text::make($media->file_name)
+                                //                     ->label($media->file_name)
+                                //                     ->url($media->getUrl());
+                                //             }
+                                //         })->all();
+
+                                //         return $list;
+                                //     })->extraAttributes([
+                                //         // 'class' => 'flex gap-2',
+                                //     ]),
+
+                                //     // Infolists\Components\SpatieMediaLibraryImageEntry::make('theses')
+                                //     //     ->collection('theses')
+                                // ]),
+                        ])
+                        ->columns(1),
                     Schemas\Components\Section::make('状态信息')
                         ->schema([
                             Infolists\Components\TextEntry::make('journal')
@@ -78,7 +153,7 @@ class ThesisInfolist
                                 ->label('状态')
                                 ->iconPosition(IconPosition::After),
                         ])
-                            
+                        ->grow(false)
                 ])
                 ->columnSpanFull()
                 ->from('lg'),
