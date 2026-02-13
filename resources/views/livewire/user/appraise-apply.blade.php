a487e97b44c8761cc458074b5cd6d583423abf5b
smallnews@qq.com <1371606921@qq.com>
2026-02-13 17:17:24 +0800

    优化 面包屑导航样式

--- resources/views/livewire/user/appraise-apply.blade.php
+++ resources/views/livewire/user/appraise-apply.blade.php
@@ -7,7 +7,7 @@
 <x-dynamic-component :component="$this->getPageContainer()" :scope-type="$scopeType" :scope-id="$scopeId">
     <div class="container mx-auto flex flex-col grow gap-4 my-4">
         @if($breadcrumbs)
-            <div class="w-full flex items-center gap-2 text-sm text-gray-500 text-left">
+            <div class="sn-descript-text w-full flex items-center gap-2 text-left">
                 当前位置 :
                 <x-sn-support::breadcrumbs :breadcrumbs="$breadcrumbs" />
             </div>
